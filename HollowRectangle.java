2aca462a998644eef082c0c86932ee5e48675377
PRADIPTI SINGH <91623073+Pradipti-Singh@users.noreply.github.com>
2022-10-14 21:51:57 +0530

    Add initial files

---
+++ HollowRectangle.java
@@ -0,0 +1,25 @@
+import java.net.StandardSocketOptions;
+import java.util.*;
+
+public class Patterns {
+    public static void main(String args []){
+        int n = 4;
+        int m = 5;
+
+        //outer loop
+        for(int i = 1; i<=n; i++){
+            //inner loop
+            for(int j = 1; j<=m; j++){
+                if
+                    (i == 1 || j == 1 || i == n || j == m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
